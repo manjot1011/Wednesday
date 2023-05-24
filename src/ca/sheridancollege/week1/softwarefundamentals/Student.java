0f5fde1ae8f708774a026c0986cf503259ff8c88
Manjot Singh <133162116+manjot1011@users.noreply.github.com>
2023-05-24 14:32:36 -0400

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -15,7 +15,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student 
 {
     private String name;
-    private String lastName;
+    private String middleName;
 
     /**
      * @return the name
